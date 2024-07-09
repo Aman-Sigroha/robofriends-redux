9e56545172f153ddb73707565510ac4dfdf005d4
Aman-Sigroha <amansigroha4@gmail.com>
2024-07-09 18:27:30 +0530

    uploading files

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import 'tachyons';
+import App from './containers/App';
+import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import { createStore , applyMiddleware, combineReducers} from 'redux';
+import { searchRobots, requestRobots } from './reducers';
+import { createLogger } from 'redux-logger';
+import {thunk} from 'redux-thunk';
+
+const rootReducer = combineReducers({searchRobots, requestRobots})
+const logger = createLogger();
+const store = createStore(rootReducer, applyMiddleware(thunk, logger));
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
